3baf2f1221edb362d9b6be6d7d011e8ea6d22d22
lwkforevel <13261202527@163.com>
2019-07-20 22:34:45 +0800

    加密加强测试

--- src/main/java/time/no/see/test/DataAddMapper.java
+++ src/main/java/time/no/see/test/DataAddMapper.java
@@ -1,12 +1,17 @@
 package time.no.see.test;
 
+import java.security.SecureRandom;
+
 import org.apache.shiro.crypto.RandomNumberGenerator;
 import org.apache.shiro.crypto.SecureRandomNumberGenerator;
 import org.apache.shiro.crypto.hash.DefaultHashService;
+import org.apache.shiro.crypto.hash.Hash;
 import org.apache.shiro.crypto.hash.HashRequest;
 import org.apache.shiro.crypto.hash.SimpleHash;
 import org.apache.shiro.util.ByteSource;
 import org.apache.shiro.util.SimpleByteSource;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,53 +31,63 @@ public class DataAddMapper {
 		
     private RandomNumberGenerator randomNumberGenerator = new SecureRandomNumberGenerator();
 
-    private String algorithmName = "md5";
-    private final int hashIterations = 2;
-    
+    private String algorithmName = "MD5";
+    private int hashIterations = 2;
+	private long id = 2;
+	private String username = "visitor";
+	private String password = "royal";
+	private String salt = null;
+	private String Dpassword = null;
+	
+	private DefaultHashService defaultHashService;
+	
+	
 	   @Autowired
 	   Sys_userMapper sys_userMapper;
-	
-	   @Test
-	//   @Transactional
-	    public void testgetMessage() throws Exception {
-		/*
-		 * Sys_user user = new Sys_user(); user.setOrganizationId((long)1);
-		 * user.setLocked(false); user.setRoleIds("1,2,3,4");
-		 * user.setUsername("admin1"); user.setPassword("admin1");
-		 * user.setSalt("100000000");
-		 */
-		  Sys_user user = sys_userMapper.selectByPrimaryKey((long)1);
-			String salt = randomNumberGenerator.nextBytes().toHex();
-			
-		   user.setUsername("life");
-		   user.setId((long)3);
-		   user.setSalt(salt);
-			DefaultHashService hashService = new DefaultHashService(); //默认算法SHA-512
-	    	hashService.setHashAlgorithmName("SHA-512");
-	    	hashService.setPrivateSalt(new SimpleByteSource("salt")); //私盐，默认无
-			/*
-			 * hashService.setGeneratePublicSalt(true);//是否生成公盐，默认false
-			 * hashService.setRandomNumberGenerator(new
-			 * SecureRandomNumberGenerator());//用于生成公盐。默认就这个
-			 */
-
-	    	 hashService.setHashIterations(hashIterations); //生成Hash值的迭代次数
-			 HashRequest request = new HashRequest.Builder()
-	    	            .setAlgorithmName(algorithmName).setSource(ByteSource.Util.bytes("royal"))
-	    	            .setSalt(ByteSource.Util.bytes(salt)).setIterations(hashIterations).build();
-			 
-			/*
-			 * String hex = hashService.computeHash(request).toHex(); String encryValue =
-			 * new SimpleHash( algorithmName, origin, ByteSource.Util.bytes(key),
-			 * hashIterations).toHex();
-			 */
-		    String hex = hashService.computeHash(request).toHex();
-		    user.setPassword(hex);
-			  Sys_user users = sys_userMapper.selectByPrimaryKey((long)3);
-			  System.out.println(hex);
-			  System.out.println(users.getPassword());
-			  boolean equals = users.getPassword().equals(hex);
-			  System.out.println(equals);
+	   
+	   @Before
+	   public void initDefaultHashService() {
+		   defaultHashService = new DefaultHashService();
+		   defaultHashService.setHashAlgorithmName(algorithmName);
+		   defaultHashService.setHashIterations(hashIterations);
+		   defaultHashService.setPrivateSalt(ByteSource.Util.bytes(username));
+		   
 	   }
-	
+	   		
+	   		@Test
+		    public void testPasswordAdd() throws Exception {
+		   		String salt = randomNumberGenerator.nextBytes().toHex();
+				HashRequest request = new HashRequest.Builder()
+		    	            .setSource(ByteSource.Util.bytes(password))
+		    	            .setSalt(ByteSource.Util.bytes(salt))
+		    	            .build();
+			    String hex = defaultHashService.computeHash(request).toHex();
+			    System.out.println("username："+username);
+			    System.out.println("publicSalt："+ salt);
+			    System.out.println("password："+ hex);
+		   }
+	   
+	   		
+	   @After
+	   public void testPassword() {
+			Sys_user user = sys_userMapper.selectByPrimaryKey(id);
+			String password = user.getPassword();
+			String encryPassword = encryPassword(password);
+			boolean equals = Dpassword.equals(encryPassword);
+			System.out.println(equals);
+	   }
+	   
+	   
+	   private String encryPassword(String password) {
+  			String Lsalt  = salt == null? randomNumberGenerator.nextBytes().toHex() : salt;
+			HashRequest request = new HashRequest.Builder()
+	    	            .setSource(ByteSource.Util.bytes(password))
+	    	            .setSalt(ByteSource.Util.bytes(Lsalt))
+	    	            .build();
+			String hex = defaultHashService.computeHash(request).toHex();
+			Dpassword = hex;
+  			return hex;
+  		}
+  	
+	   
 }
